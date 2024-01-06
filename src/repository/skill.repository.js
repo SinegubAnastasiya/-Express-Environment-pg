d91cf0f6a84ff8ace734140a35541d5149b2438f
SinegubAnastasiya <fedukevich98@mail.ru>
2024-01-06 11:30:30 +0300

    added some remarks

--- src/repository/skill.repository.js
+++ src/repository/skill.repository.js
@@ -1,13 +1,5 @@
 const pool = require('../db')
 
-// async function getAllSkillDB() {
-//     const client = await pool.connect()
-//     const sql = 'SELECT * FROM environment'
-//     const data = (await client.query(sql)).rows
-
-//     return data
-// }
-
 async function getAllSkillDB() {
     const connection = await pool.getConnection()
     const sql = 'SELECT * FROM environment'
@@ -15,14 +7,6 @@ async function getAllSkillDB() {
 
     return data
 }
-
-// async function createSkillDB(label, category, priority) {
-//     const client = await pool.connect()
-//     const sql = 'INSERT INTO environment(label, category, priority) VALUES($1, $2, $3) RETURNING *'
-//     const { rows } = await client.query(sql, [label, category, priority])
-
-//     return rows
-// }
 
 async function createSkillDB(label, category, priority) {
     const connection = await pool.getConnection()
@@ -71,3 +55,22 @@ async function getSkillByIdDB(id) {
 }
 
 module.exports = { getAllSkillDB, createSkillDB, getSkillByIdDB, updateSkillDB, deleteSkillDB, updateBodyDB }
+
+
+// async function getAllSkillDB() {
+//     const client = await pool.connect()
+//     const sql = 'SELECT * FROM environment'
+//     const data = (await client.query(sql)).rows
+
+//     return data
+// }
+
+
+
+// async function createSkillDB(label, category, priority) {
+//     const client = await pool.connect()
+//     const sql = 'INSERT INTO environment(label, category, priority) VALUES($1, $2, $3) RETURNING *'
+//     const { rows } = await client.query(sql, [label, category, priority])
+
+//     return rows
+// }
